b6d04deffcc9a3919f494743a95b395cc340773a
Ian Ramos <i.am.ian.ramos@gmail.com>
2017-10-03 15:53:36 -0700

    guide to code splitting, caching, and starting initial react environment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 import '../styles/style.css';
 
-console.log('testing');
+ReactDOM.render(<App />, document.getElementById('root'));
